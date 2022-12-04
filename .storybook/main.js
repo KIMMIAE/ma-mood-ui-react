29c62b8d1b3f9a97d1beb3151e5912389d168c5d
kimmiae <32053142+KIMMIAE@users.noreply.github.com>
2022-12-04 22:05:58 +0900

    feat: set storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,17 @@
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+      "../src/**/*.stories.mdx",
+      "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+      "@storybook/addon-links",
+      "@storybook/addon-essentials",
+      "@storybook/addon-interactions",
+      // 추가
+      "@storybook/preset-scss"
   ],
   "framework": "@storybook/react",
   "core": {
-    "builder": "@storybook/builder-webpack5"
+      "builder": "@storybook/builder-webpack5"
   }
 }
